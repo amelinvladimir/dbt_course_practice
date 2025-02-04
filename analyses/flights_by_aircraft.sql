8ddfb46a2fe281b4b4b19fc6f87f9cbb082635b1
amelinvladimir <amelinvd@gmail.com>
2025-02-04 21:08:25 +0300

    jinja

--- analyses/flights_by_aircraft.sql
+++ analyses/flights_by_aircraft.sql
@@ -1,7 +1,19 @@
-{% set important_aircrafts = ['CN1', 'CR2', '763'] %}
+{% set aircraft_query %}
+SELECT DISTINCT
+    aircraft_code
+FROM
+    {{ ref('fct_fligths') }}
+{% endset %}  
+
+{% set aircraft_query_result = run_query(aircraft_query) %}
+{% if execute %}
+    {% set important_aircrafts = aircraft_query_result.columns[0].values() %}
+{% else %}
+    {% set important_aircrafts = [] %}
+{% endif %}
 
 SELECT 
-    {% for aircraft in important_aircrafts -%}
+    {% for aircraft in important_aircrafts %}
     SUM(CASE WHEN aircraft_code = '{{ aircraft }}' THEN 1 ELSE 0 END) as fligths_{{ aircraft }} 
         {%- if not loop.last %},{% endif %}
     {%- endfor %}
